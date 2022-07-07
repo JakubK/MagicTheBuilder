eda11e91117a43eca36c37e7f7a24a330a789f92
Jakub Kuniszewski <you@example.com>
2022-07-07 14:56:42 +0200

    Install sass and register first empty route

---
+++ mtb-frontend/src/router/index.ts
@@ -0,0 +1,21 @@
+import HelloWorld from "@/components/HelloWorld.vue";
+import Cards from "@/views/Cards.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        component: HelloWorld
+    },
+    {
+        path: '/cards',
+        component: Cards
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
---
+++ mtb-frontend/src/views/Cards.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>xD</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped src="./Cards.scss"></style>

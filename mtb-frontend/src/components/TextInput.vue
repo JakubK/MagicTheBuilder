be201776fe54ec7895150d30e124487080220024
Jakub Kuniszewski <you@example.com>
2022-07-07 15:59:48 +0200

    Create basic TextInput component with 2 named slots

---
+++ mtb-frontend/src/components/TextInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="textinput" :class="{ textinput__focused: focused }">
+    <div class="textinput__slot">
+      <slot name="left"></slot>
+    </div>
+    <input @focus="focused = true" @blur="focused = false" class="textinput__input" :placeholder="placeholder"/>
+    <div>
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  defineProps({
+    placeholder: String,
+  });
+
+  const focused = ref<boolean>(false);
+</script>
+
+<style lang="scss" scoped src="./TextInput.scss"/>

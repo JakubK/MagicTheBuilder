6028d8f0171bfe0d6f373472307ff5445ce6d718
Jakub Kuniszewski <you@example.com>
2022-07-31 15:33:48 +0200

    Install Vuelidate and add validation for Register and Login views

--- mtb-frontend/src/views/Login.vue
+++ mtb-frontend/src/views/Login.vue
@@ -2,23 +2,50 @@
   <div class="login">
     <h2>Logging into existing account</h2>
     <div class="login__form">
-      <TextInput placeholder="E-mail address"></TextInput>
+      <TextInput v-model="form.email" placeholder="E-mail address"></TextInput>
       <br />
-      <TextInput type="password" placeholder="Password"></TextInput>
+      <TextInput v-model="form.password" type="password" placeholder="Password"></TextInput>
       <br />
       <div class="login__remember">
-        <CheckBox label="Remember me"/>
+        <CheckBox v-model="form.rememberMe" label="Remember me"/>
         <span>Forgot your password?</span>
       </div>
       <br />
-      <button>Sign in</button>
+      <button @click="submitLogin">Sign in</button>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { reactive } from 'vue';
+import useVuelidate from '@vuelidate/core';
+import { required, email } from '@vuelidate/validators'
+
 import TextInput from '@/components/TextInput.vue';
 import CheckBox from '@/components/CheckBox.vue';
+import { SignIn } from '@/models/signIn';
+
+const form = reactive<SignIn>({
+  email: '',
+  password: '',
+  rememberMe: false
+});
+
+const rules = {
+  email: {
+    required, email
+  },
+  password: required,
+}
+
+const v$ = useVuelidate(rules, form);
+
+const submitLogin = async() => {
+  const isValid = await v$.value.$validate();
+  if(isValid) {
+    //  Send the actual form
+  }
+}
 
 </script>
 
--- mtb-frontend/src/views/Register.vue
+++ mtb-frontend/src/views/Register.vue
@@ -1,26 +1,63 @@
 <template>
   <div class="register">
     <h2>Account creation</h2>
-    <TextInput placeholder="E-mail address"></TextInput>
+    <TextInput v-model="form.email" placeholder="E-mail address"/>
     <br />
-    <TextInput type="password" placeholder="Password"></TextInput>
+    <TextInput v-model="form.password" type="password" placeholder="Password"/>
     <br />
-    <TextInput type="password" placeholder="Repeat password"></TextInput>
+    <TextInput v-model="form.passwordRepeat" type="password" placeholder="Repeat password"/>
     <br />
     <div class="register__agreements">
-      <CheckBox label="I want to get email notifications about changes in the service" />
+      <CheckBox v-model="form.allowNotifications" label="I want to get email notifications about changes in the service" />
       <br/>
-      <CheckBox label="I accept that my data will be stored and processed by this service" />
+      <CheckBox v-model="form.allowDataProcessing" label="I accept that my data will be stored and processed by this service" />
     </div>
     <br />
-    <button>Sign up</button>
+    <button @click="submitRegister">Sign up</button>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { reactive } from 'vue';
+import useVuelidate from '@vuelidate/core';
+import { required, email } from '@vuelidate/validators';
+
 import TextInput from '@/components/TextInput.vue';
 import CheckBox from '@/components/CheckBox.vue';
+import { SignUp } from '@/models/signUp';
+import { areSame, mustBeTrue } from '@/utils/valitators';
 
+const form = reactive<SignUp>({
+  email: '',
+  password: '',
+  passwordRepeat: '',
+  allowDataProcessing: false,
+  allowNotifications: false
+});
+
+
+const rules = {
+  email: {
+    required, email
+  },
+  password: required,
+  passwordRepeat: { 
+    required,
+    sameAs: areSame(form.password, form.passwordRepeat)
+  },
+  allowDataProcessing: {
+    mustBeTrue,
+  }
+}
+
+const v$ = useVuelidate(rules, form);
+
+const submitRegister = async() => {
+  const isValid = await v$.value.$validate();
+  if(isValid) {
+    //  Send the actual form
+  } 
+}
 
 </script>
 

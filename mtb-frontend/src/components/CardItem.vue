32362b6c51668b4a032db472e90b6116359805e2
Jakub Kuniszewski <you@example.com>
2022-09-11 20:25:00 +0200

    Add card id props and add base for Decks view

--- mtb-frontend/src/components/CardItem.vue
+++ mtb-frontend/src/components/CardItem.vue
@@ -15,17 +15,19 @@
         <footer>
           <div class="card-reverse__actions">
             <div class="card-reverse__action">
-              <Icon>
-                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M16.0001 29.3333C23.3639 29.3333 29.3334 23.3638 29.3334 16C29.3334 8.63621 23.3639 2.66667 16.0001 2.66667C8.63628 2.66667 2.66675 8.63621 2.66675 16C2.66675 23.3638 8.63628 29.3333 16.0001 29.3333Z"
-                    stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  <path d="M16 21.3333V16" stroke="white" stroke-width="2" stroke-linecap="round"
-                    stroke-linejoin="round" />
-                  <path d="M16 10.6667H16.0133" stroke="white" stroke-width="2" stroke-linecap="round"
-                    stroke-linejoin="round" />
-                </svg>
-              </Icon>
+              <router-link :to="`/cards/${id}`">
+                <Icon>
+                  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M16.0001 29.3333C23.3639 29.3333 29.3334 23.3638 29.3334 16C29.3334 8.63621 23.3639 2.66667 16.0001 2.66667C8.63628 2.66667 2.66675 8.63621 2.66675 16C2.66675 23.3638 8.63628 29.3333 16.0001 29.3333Z"
+                      stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M16 21.3333V16" stroke="white" stroke-width="2" stroke-linecap="round"
+                      stroke-linejoin="round" />
+                    <path d="M16 10.6667H16.0133" stroke="white" stroke-width="2" stroke-linecap="round"
+                      stroke-linejoin="round" />
+                  </svg>
+                </Icon>
+              </router-link>
             </div>
             <Icon @click="flipped = !flipped" class="card-reverse__flip">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -74,6 +76,14 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import Icon from './Icon.vue';
+
+defineProps({
+  id: {
+    type: Number,
+    required: false,
+    default: 1
+  }
+});
 
 const flipped = ref(false);
 </script>
---
+++ mtb-frontend/src/views/Decks.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="decks-view">
+    <BaseHeader>{{ title }}</BaseHeader>
+    <div class="decks-view__decks">
+      <CardItem class="cards-view__card" v-for="index in 10" :key="index" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BaseHeader from '@/components/typography/BaseHeader.vue';
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>

d590b1ad997309973a5f6ee993359f5f1397f154
Jakub Kuniszewski <you@example.com>
2022-11-01 20:41:49 +0100

    Attach jwt to each request to API

--- mtb-frontend/src/services/api/index.ts
+++ mtb-frontend/src/services/api/index.ts
@@ -4,4 +4,13 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 });
 
+api.interceptors.request.use(request => {
+    if(localStorage.getItem('jwt')) {
+        const token = localStorage.getItem('jwt');
+        request.headers!.Authorization = `${token}`;
+    }
+
+    return request;
+});
+
 export default api;
